15a8ad2d3f4dbbaeb5d18a8482e742b77dfb8368
Mahmoud Sahebi <msa.prog@gmail.com>
2024-01-08 13:56:10 +0330

    feat(ui): add file-list to home-page

--- src/views/pages/home/home.page.tsx
+++ src/views/pages/home/home.page.tsx
@@ -1,14 +1,32 @@
+import { css } from "hono/css";
 import { MainLayout } from "#src/views/layouts/main/main.layout";
+import { fileListCache } from "#src/routes/files.route";
+
+const fileListClass = css`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  font-family: monospace;
+`;
 
 export function HomePage() {
+  const fileList = Array.from(fileListCache);
+
   return (
     <>
       <h1>Simple File Storage</h1>
-      <form action="/api/files" encType="multipart/form-data" method="POST" target="dummyframe">
-        <iframe name="dummyframe" id="dummyframe" style="display: none;" />
+      <form action="/api/files" encType="multipart/form-data" method="POST">
         <input type="file" id="files" name="files" />
         <input type="submit" />
       </form>
+      <h2>Files</h2>
+      <div class={fileListClass}>
+        {fileList.map((f) => (
+          <a key={f} href={`/api/files/${f}`}>
+            {f}
+          </a>
+        ))}
+      </div>
     </>
   );
 }
