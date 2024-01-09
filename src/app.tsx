a3ad6b32337be2d44421c1801c71adf5fe509bc0
Mahmoud Sahebi <msa.prog@gmail.com>
2024-01-09 11:35:24 +0330

    docs: add comments to app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -9,14 +9,17 @@ import { HomePage } from "#src/views/pages/home/home.page";
 import { env } from "#src/data/env";
 import { messages } from "#src/data/messages/en";
 
+/// App init
 export const app = new Hono();
-app.use("*", prettyJSON());
-app.notFound((c) => c.json({ message: messages.error.NOT_FOUND }, 404));
-app.onError((err, c) => {
-  console.error(err);
-  return c.json({ message: messages.error.INTERNAL_SERVER_ERROR }, 500);
-});
+app
+  .use("*", prettyJSON())
+  .notFound((c) => c.json({ message: messages.error.NOT_FOUND }, 404))
+  .onError((err, c) => {
+    console.error(err);
+    return c.json({ message: messages.error.INTERNAL_SERVER_ERROR }, 500);
+  });
 
+/// Home page route
 app.get("/", (c) =>
   c.html(
     <HomePage.layout>
@@ -25,23 +28,26 @@ app.get("/", (c) =>
   ),
 );
 
+/// Main API init
 const api = new OpenAPIHono({
   defaultHook: (result, c) => {
     if (!result.success) return c.json({ message: "Invalid request" }, 400);
   },
 });
+api.use("*", cors()).route("/files", filesRoute);
 if (env.VERBOSE) {
   api.use("*", logger());
 }
-api.use("*", cors());
-api.route("/files", filesRoute);
 
+/// OpenAPI docs and Swagger UI
 api.doc31("/openapi.json", {
   openapi: "3.1.0",
   info: { version: "1.0.0", title: "Simple File Storage API" },
 });
 api.get("/docs", swaggerUI({ url: "/api/v1/openapi.json" }));
 
+/// Whole File Storage API type to be used in frontend projects
 export type FileStorageApi = typeof api;
 
+/// Mount the API
 app.route("/api/v1", api);
