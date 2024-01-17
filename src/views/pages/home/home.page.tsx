9af60165d625c0728d9f70043497a94b0261cc61
Mahmoud Sahebi <msa.prog@gmail.com>
2024-01-17 14:13:53 +0330

    feat: improve styles

--- src/views/pages/home/home.page.tsx
+++ src/views/pages/home/home.page.tsx
@@ -14,17 +14,17 @@ export function HomePage() {
         method="POST"
       >
         <input type="file" name="files" multiple="true" />
-        <input type="submit" class="btn-blue" />
+        <input type="submit" class="btn" />
       </form>
       <h2>Files</h2>
       <div class="flex flex-col gap-1 font-mono">
         {fileList.map((f) => {
           const divId = f.replace(".", "-");
           return (
-            <div key={divId} id={divId} class="flex gap-1">
+            <div key={divId} id={divId} class="flex gap-2 items-end">
               <a href={`/api/v1/files/${f}`}>{f}</a>
               <button
-                class="btn-blue"
+                class="btn bg-red-500 text-white"
                 hx-delete={`/api/v1/files/${f}`}
                 hx-target={"closest div"}
                 hx-swap={"delete"}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /* eslint-disable -- config */
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   important: "#app",
   content: ["./src/**/*.{html,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
